18df3025b39d741735c6b21bff033b6a583e90d4
Rodrigo Leon <rleonblandon@gmail.com>
2022-07-08 16:49:50 -0600

    Created rate global var to minimize HTTP requests

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
+from sqlite3 import DateFromTicks
 from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
-from datetime import date
+from datetime import date, datetime
 
 app = Flask(__name__)
 
+# Static variable to hold latest exchange rate.
+latest_rate = 1.0
+latest_update = date(1900, 1, 1)
+
 def get_rate():
-   bcn_source = requests.request("GET", "https://www.bcn.gob.ni/").content
-   soup = BeautifulSoup(bcn_source, 'html.parser')
-   table_cell = soup.select("#economicos tr:nth-of-type(5) td:nth-of-type(1)")[0]
-   cell_value = table_cell.get_text()
-   exchange_rate = float(cell_value[1:8])
-   return exchange_rate
+   global latest_update
+   global latest_rate
+   if (latest_update < date.today()):
+      print('Fetching fresh date from BCN.gob.ni ...')
+      bcn_source = requests.request("GET", "https://www.bcn.gob.ni/").content
+      soup = BeautifulSoup(bcn_source, 'html.parser')
+      table_cell = soup.select("#economicos tr:nth-of-type(5) td:nth-of-type(1)")[0]
+      cell_value = table_cell.get_text()
+      exchange_rate = float(cell_value[1:8])
+      latest_rate = exchange_rate
+      latest_update = date.today()
+   return latest_rate
 
 @app.route("/", methods=['GET','POST'])
 def homepage():
